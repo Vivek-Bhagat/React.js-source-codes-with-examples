c5cf02606b5b7b8238ecac05fe84544093072657
Vivek Bhagat <vivekbhagat1890@hotmail.com>
2023-03-25 14:26:05 +0530

    class and function component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import React, { Component } from 'react'
+// import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Goodbye from './components/Goodbye';
+class App extends Component {
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Greet />
+        <Welcome />
+        <Goodbye />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Goodbye.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Goodbye = () => <h1>See you Later Buddy</h1>
+
+
+export default Goodbye
