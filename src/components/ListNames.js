275cd1915005e202e80f8f101933fe29bc03a59f
Vivek <vivekbhagat1890@hotmail.com>
2023-05-14 16:39:41 +0530

    personsNames fectin usung map array

--- src/components/ListNames.js
+++ src/components/ListNames.js
@@ -23,11 +23,44 @@ import React from 'react'
 //}
 
 // !  Simple format for rendering list of names
+// function ListNames() {
+//     const name = ['Bruce', 'Diana', 'Clark']
+//     const className = name.map(names => <h2>{names}</h2>)
+//     return (
+//         <div>{className}</div>
+//     )
+// }
+
 function ListNames() {
-    const name = ['Bruce', 'Diana', 'Clark']
-    const className = name.map(names => <h2>{names}</h2>)
+    const person = [
+        {
+            id: 1,
+            name: 'Sid',
+            age: '19',
+            skill: 'react'
+        },
+        {
+            id: 2,
+            name: 'Mayce',
+            age: '18',
+            skill: 'angular'
+        },
+        {
+            id: 3,
+            name: 'Bruce',
+            age: '21',
+            skill: 'Java'
+        },
+        {
+            id: 4,
+            name: 'Tony',
+            age: '17',
+            skill: 'C++'
+        },
+    ]
+    const personName = person.map(person => <h2> I'm {person.name}, and I'm {person.age} years old. My skill is {person.skill}.</h2>)
     return (
-        <div>{className}</div>
+        <div>{personName}</div>
     )
 }
 
