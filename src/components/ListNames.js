40bffcfe893eb379f462fbd7f34ea100cf76ff17
Vivek <vivekbhagat1890@hotmail.com>
2023-05-14 16:12:11 +0530

    Simple format for rendering list name/items in jsx

--- src/components/ListNames.js
+++ src/components/ListNames.js
@@ -1,24 +1,33 @@
 import React from 'react'
 
+// function ListNames() {
+// const name = ['Bruce', 'Diana', 'Clark']
+// /return (
+// *simple method of rendering list from an array
+// <div>
+//       <h2>{name[0]}</h2>
+//       <h2>{name[1]}</h2>
+//       <h2>{name[2]}</h2>
+// </div>
+
+// * rendering array elemanr in list using map method
+// <div>
+// {
+// functionName.map('name' => <span>{name}</span>)
+// name.map(list => <h2>{list}</h2>)
+// }
+// {/* </div> */}
+
+
+// )
+//}
+
+// !  Simple format for rendering list of names
 function ListNames() {
     const name = ['Bruce', 'Diana', 'Clark']
+    const className = name.map(names => <h2>{names}</h2>)
     return (
-        // *simple method of rendering list from an array
-        // <div>
-        //       <h2>{name[0]}</h2>
-        //       <h2>{name[1]}</h2>
-        //       <h2>{name[2]}</h2>
-        // </div>
-
-        // * rendering array elemanr in list using map method
-        <div>
-            {
-                // functionName.map('name' => <span>{name}</span>)
-                name.map(list => <h2>{list}</h2>)
-            }
-
-        </div>
-
+        <div>{className}</div>
     )
 }
 
