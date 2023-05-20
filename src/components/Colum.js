98f1f4bcca077fd279e0bc59ec2da328e26811b6
Vivek <vivekbhagat1890@hotmail.com>
2023-05-21 01:51:34 +0530

    key in fragment

--- src/components/Colum.js
+++ src/components/Colum.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 function Colum() {
+    const items =[]
   return (
     // * by using fragment we can avoid the warning error in the console.
     <React.Fragment>
+        {
+            items.map( items =>(
+                <React.Fragment key={items.id}>
+                    <h1>Title</h1>
+                    <p>{items.map}</p>
+                    </React.Fragment>
+            ))
+        }
+        
       <td>User</td>
       <td>Guest</td>
     </React.Fragment>
